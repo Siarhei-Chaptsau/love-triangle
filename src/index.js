5c83b8ec5b07f6e6c3020f618c6f8d7d3a411285
Siarhei <s.chaptsau@gmail.com>
2018-02-23 16:17:09 +0300

    calculated love triangles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
- */
+*/
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count  = 0;
+  preferences.forEach(function(item, i, arr) {
+    var first = arr[i] - 1;
+    var second = arr[first] - 1;
+    var third = arr[second] - 1;
+
+    if (third === i && first !== i && second !== i) {
+      count += 1;
+      arr[i] = -1;
+    }
+  });
+  return count;
 };
